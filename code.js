9bfc38efb4ad6f681c18ffdc2f9edb127937fbd7
gaygall <64158404+gaygall@users.noreply.github.com>
2020-04-25 00:20:39 +0600

    Add files via upload

--- code.js
+++ code.js
@@ -1,34 +1,37 @@
 var gallery = document.getElementById("gall");
-		var photos  = ["art/1.jpg"
-					  ,"art/2.jpg"
-					  ,"art/3.jpg"
-					  ,"art/4.jpg"
-					  ,"art/5.jpg"
-					  ,"art/6.jpg"
-					  ,"art/7.jpg"
-					  ,"art/8.jpg"
-					  ,"art/9.jpg"
-					  ,"art/10.png"
-					  ,"art/11.jpg"
-					  ,"art/12.jpg"
-				]
-		var sel= 0;
-
-		window.onload = main();
-		function main(){
-			gallery.src = photos[sel];
-		}
-		function left(){
-			sel--
-			if(sel<0){
-				sel = photos.length-1;
-			}
-			main();
-		}
-		function right(){
-			sel++
-			if(sel >= photos.length){
-				sel = 0;
-			}
-			main()
-		}
+var photos  = ["art/1.jpg"
+			  ,"art/2.jpg"
+			  ,"art/3.jpg"
+			  ,"art/4.jpg"
+			  ,"art/5.jpg"
+			  ,"art/6.jpg"
+			  ,"art/7.jpg"
+			  ,"art/8.jpg"
+			  ,"art/9.jpg"
+			  ,"art/10.png"
+			  ,"art/11.jpg"
+			  ,"art/12.jpg"
+		]
+var sel= 0;
+var image = new Image();
+for(var i = 0;i<photos.length-1;i++){
+	image.src = photos[i]
+}
+image.onload = main();
+function main(){
+	gallery.src = photos[sel];
+}
+function left(){
+	sel--
+	if(sel<0){
+		sel = photos.length-1;
+	}
+	main();
+}
+function right(){
+	sel++
+	if(sel >= photos.length){
+		sel = 0;
+	}
+	main()
+}
